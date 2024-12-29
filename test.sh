4ca78ad394cf4f12b77f24acd32ebba0af783da7
Jasmijn Wellner <gyvox.public@gmail.com>
2024-12-29 16:02:15 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
-lua_install/bin/luacheck --std max+busted bitser.lua spec --globals love
+lua_install/bin/luacheck --std max+busted bitser.lua spec --globals love --no-max-line-length
 lua_install/bin/busted --verbose --coverage
 lua_install/bin/luacov bitser$ bitser_spec$
