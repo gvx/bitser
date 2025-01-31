cbdec4974fe48fd847860961a02866a8de033cf5
R.E.J. Wellner <gyvox.public@gmail.com>
2025-01-31 16:48:25 +0100

    Update setuptest.sh
    Don't add to PATH for just a couple of commands in a script.

--- setuptest.sh
+++ setuptest.sh
@@ -1,11 +1,10 @@
 python3 -m pip install hererocks
 python3 -m hererocks lua_install -r^ --luajit=2.1
-export PATH=$PATH:$PWD/lua_install/bin
-luarocks install luacheck 
-luarocks install busted
-luarocks install luacov
-luarocks install luacov-coveralls
-luarocks install middleclass
 wget https://raw.githubusercontent.com/bartbes/slither/b9cf6daa1e8995093aa80a40ee9ff98402eeb602/slither.lua
 wget https://raw.githubusercontent.com/vrld/hump/038bc9025f1cb850355f4b073357b087b8122da9/class.lua
 wget https://raw.githubusercontent.com/rxi/classic/e5610756c98ac2f8facd7ab90c94e1a097ecd2c6/classic.lua
+lua_install/bin/luarocks install luacheck
+lua_install/bin/luarocks install busted
+lua_install/bin/luarocks install luacov
+lua_install/bin/luarocks install luacov-coveralls
+lua_install/bin/luarocks install middleclass
